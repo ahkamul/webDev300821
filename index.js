21af53244e31488742cc4084edabd12b61fcaf39
ahkamul <sasraishougat@gmail.com>
2021-09-06 10:57:26 +0600

    Code written on js in index.js at Sep 6 inspite of creating Repo at Aug 30 with writing code in index.html

---
+++ index.js
@@ -0,0 +1,30 @@
+// var obj = {}
+// //console.log(obj)
+// //console.log(typeof obj)
+
+// obj.x = 10;
+
+// console.log(obj)
+
+// JS Object Literal 
+
+// var point = {
+//     x : 10,
+//     y : 20
+// }
+// //point.y = 30;
+// point.z = 30;
+// point.m = 50;
+
+// console.log(point)
+
+// JS Object Constructor
+
+var obj = Object()
+obj.a = 10
+
+console.log(obj)
+
+var obj2 = new Object()
+obj2.b = 20
+console.log(obj2)
